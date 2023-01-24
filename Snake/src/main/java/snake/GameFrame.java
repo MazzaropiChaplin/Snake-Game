c9c2fdba5b3131605846947fbdb5437877cf0040
Roberto Carlos <121329916+MazzaropiChaplin@users.noreply.github.com>
2023-01-24 05:20:32 -0300

    Add files via upload

---
+++ Snake/src/main/java/snake/GameFrame.java
@@ -0,0 +1,18 @@
+package snake;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+    GameFrame(){
+        
+        
+        this.add(new GamePane());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        
+    }
+}
---
+++ Snake/src/main/java/snake/Snake.java
@@ -0,0 +1,11 @@
+
+
+package snake;
+
+public class Snake {
+
+    public static void main(String[] args) {
+        
+        new GameFrame();
+    }
+}
